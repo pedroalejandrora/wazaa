35dfacc0a2eb5a97f0f40e3c4ac0eb27afc36f14
pedroalejandrora <157652938+pedroalejandrora@users.noreply.github.com>
2024-01-24 16:34:03 -0600

    Create waza.py

---
+++ waza.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL":"una respuesta a una broma"
+            }
+            
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Todavía no tenemos esta palabra... Pero estamos trabajando en ella.")
